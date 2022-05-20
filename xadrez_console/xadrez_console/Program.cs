a41ee4cba0d2bd87446c6dfa2219dc1445eda670
chris-lock2 <chris.lock1998@gmail.com>
2022-05-20 14:52:16 -0300

    Adicionando inicio do projeto

---
+++ xadrez_console/xadrez_console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Posicao p = new Posicao(3,4);
+
+            Console.WriteLine("Posicao: " + p);
+            Console.ReadLine();
+        }
+    }
+}

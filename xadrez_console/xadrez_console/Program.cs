04ff25035ffffbbec258f632956923d5af46bd1a
chris-lock2 <chris.lock1998@gmail.com>
2022-05-24 17:55:45 -0300

    FEAT: add visual do campo

--- xadrez_console/xadrez_console/Program.cs
+++ xadrez_console/xadrez_console/Program.cs
@@ -8,9 +8,9 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
-            Posicao p = new Posicao(3,4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
 
-            Console.WriteLine("Posicao: " + p);
             Console.ReadLine();
         }
     }

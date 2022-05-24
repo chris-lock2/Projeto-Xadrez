84cdbf203b36869408b025a0831f8b2548ac0885
chris-lock2 <chris.lock1998@gmail.com>
2022-05-24 19:56:39 -0300

    feat: adicionando cores nas pecas

--- xadrez_console/xadrez_console/Program.cs
+++ xadrez_console/xadrez_console/Program.cs
@@ -8,33 +8,36 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            //    Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 1));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 6));
+
+                Tela.imprimirTabuleiro(tab);
 
 
-            //}
-            //catch(TabuleiroException e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
-
-            //Console.ReadLine();
-
-            PosicaoXadrez pos = new PosicaoXadrez('d', 2);
-
-            Console.WriteLine(pos);
-
-            Console.WriteLine(pos.toPosicao());
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
+
+            //PosicaoXadrez pos = new PosicaoXadrez('d', 2);
+
+            //Console.WriteLine(pos);
+
+            //Console.WriteLine(pos.toPosicao());
+
+            //Console.ReadLine();
 
         }
     }
